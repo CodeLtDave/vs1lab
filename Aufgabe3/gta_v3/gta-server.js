3f7b10dd1e5d2caed2d70d40f1f11f2b513ea2ee
dayv007 <42917573+dayv007@users.noreply.github.com>
2021-06-02 15:44:41 +0200

    ???

--- Aufgabe3/gta_v3/gta-server.js
+++ Aufgabe3/gta_v3/gta-server.js
@@ -13,13 +13,13 @@ var http = require('http');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var express = require('express');
+const { response } = require('express');
+const { body } = require('express-validator');
 
 var app;
 app = express();
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Setze ejs als View Engine
 app.set('view engine', 'ejs');
@@ -36,7 +36,14 @@ app.set('view engine', 'ejs');
  * GeoTag Objekte sollen min. alle Felder des 'tag-form' Formulars aufnehmen.
  */
 
-// TODO: CODE ERGÄNZEN
+class GeoTag{
+    constructor(body) {
+        this.latitude = body.latitude
+        this.longitude = body.longitude
+        this.name = body.name
+        this.hashtag = body.hashtag
+    }
+}
 
 /**
  * Modul für 'In-Memory'-Speicherung von GeoTags mit folgenden Komponenten:
@@ -47,7 +54,29 @@ app.set('view engine', 'ejs');
  * - Funktion zum Löschen eines Geo Tags.
  */
 
-// TODO: CODE ERGÄNZEN
+var taglist = [];
+
+function searchName(searchTerm) {
+    var indexList = [];
+    for (var i = 0; i<taglist.length; i++) {
+        if (1==1) {
+            indexList.push(i);
+        }
+    }
+    console.log("Results:" + indexList);
+    return indexList;
+}
+
+function addTag(body) {
+    taglist.push(new GeoTag(body));
+    console.log("Tagg added!");
+}
+
+function deleteTag(index) {
+    taglist.splice(index,1);
+    console.log("Tagg deleted");
+    console.log(taglist);
+}
 
 /**
  * Route mit Pfad '/' für HTTP 'GET' Requests.
@@ -77,7 +106,11 @@ app.get('/', function(req, res) {
  * Die Objekte liegen in einem Standard Radius um die Koordinate (lat, lon).
  */
 
-// TODO: CODE ERGÄNZEN START
+ app.post('/tagging', function(req, res){
+    console.log(req.body);
+    addTag(req.body);
+    res.render('gta', {taglist});
+ });
 
 /**
  * Route mit Pfad '/discovery' für HTTP 'POST' Requests.
@@ -91,7 +124,15 @@ app.get('/', function(req, res) {
  * Falls 'term' vorhanden ist, wird nach Suchwort gefiltert.
  */
 
-// TODO: CODE ERGÄNZEN
+ app.post('/discovery', function(req, res){
+    console.log("Index: " + req.body.searchTerm);
+    var indexList = searchName(req.body.searchTerm);
+    var resultList = [];
+    for (var i = 0; i<indexList; i++) {
+        resultList.push(taglist[indexList[i]]);
+    }
+    res.render('gta', {resultList});
+ });
 
 /**
  * Setze Port und speichere in Express.
